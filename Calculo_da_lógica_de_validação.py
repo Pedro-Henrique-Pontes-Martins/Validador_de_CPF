0ef7edc9cf0b14b27e47336b9694016dea1335e4
30 <pedrosenaitech@gmail.com>
2023-04-27 10:36:36 -0300

    criação do arquivo para a realização do projeto

---
+++ Calculo_da_lógica_de_validação.py
@@ -0,0 +1,20 @@
+multiplicadores = [10, 9, 8, 7, 6, 5, 4, 3, 2]
+númerosDeUmCPF = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+def verificador(cpf):
+    cpfFiltrado = []
+    for i in cpf:
+        if(i in númerosDeUmCPF):
+            cpfFiltrado.append(int(i))
+    cpfComparação = cpfFiltrado
+    cpfComparação.pop()
+    cpfComparação.pop()
+
+    somaDosNúmeros = cpfComparação
+    contador = 0
+    for i in somaDosNúmeros:
+        i *= multiplicadores[contador]
+        somaDosNúmeros.append(i)
+        contador += 1
+
+    somaDosNúmeros = sum(somaDosNúmeros)
